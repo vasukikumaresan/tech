61a37fd0ffc6b9a8c6d71cf135ef7be1b244f417
vasukikumaresan <vasukikumaresan1211@gmail.com>
2021-12-11 06:42:37 +0530

    gitlaunch

--- src/org/tcs/Employee.java
+++ src/org/tcs/Employee.java
@@ -1,13 +1,19 @@
 package org.tcs;
 
 public class Employee {
+	
+	public void test() {
+
+		System.out.println("git hub test");
+		
+	}
 
 	
 	public void sample() {
 		System.out.println("Employee name is:divya");
 
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		
 	}
 	
