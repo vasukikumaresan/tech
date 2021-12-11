2cc46a5b10117883f3c8b96929f96b3dedc61f07
vasukikumaresan <vasukikumaresan1211@gmail.com>
2021-12-11 05:33:12 +0530

    secondgit

---
+++ src/org/tcs/Employee.java
@@ -0,0 +1,14 @@
+package org.tcs;
+
+public class Employee {
+
+	
+	public void sample() {
+		System.out.println("Employee name is:divya");
+
+	}
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/org/tcs/Salary.java
@@ -0,0 +1,19 @@
+package org.tcs;
+// child
+public class Salary extends Employee {
+	
+	public void empDetail() {
+		System.out.println("employee address is omr");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Salary emp =new Salary();
+		emp. empDetail();
+		emp.sample();
+		
+	}
+	
+
+}
